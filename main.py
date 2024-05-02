daa4ca59fe7b8c2c009be93dd805ff313b669e3b
siddhikotre <siddhikotre72@gmail.com>
2024-05-02 23:55:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 from fastapi import FastAPI, HTTPException
 import requests
 from openai import OpenAI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 #RecipeRadar --Author Siddhi Kotre--
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 # Set your OpenAI API key
 api_key = "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"
 
 @app.post("/generate_recipe")
-async def generate_recipe(ingredients: list):
+async def generate_recipe(ingredients: str):
+    print(ingredients)
     # Check if ingredients are provided
     if not ingredients:
         raise HTTPException(status_code=400, detail="Please provide a list of ingredients")
-
-
+    
     # Generate recipe using OpenAI's API
     prompt = "Generate a recipe using the following ingredients(recipe):" + ", ".join(ingredients)
     try:
@@ -29,7 +40,7 @@ async def generate_recipe(ingredients: list):
             ],
         )
         
-#Get the generated text from the responses and displaying it
+        # Get the generated text from the responses and displaying it
         recipe = response.choices[0].message.content
         return {"Recipe" : recipe}
 
